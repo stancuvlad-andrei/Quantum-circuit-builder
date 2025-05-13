3e2b749bdf17a8b0833b435c519cabe5b9e5b81c
Stancu Vlad-Andrei <savandrei.402@e-uvt.ro>
2025-05-13 20:48:22 +0300

    Updated the main menu

--- Assets/Resources/Scripts/Main Menu.cs
+++ Assets/Resources/Scripts/Main Menu.cs
@@ -1,20 +1,83 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public void StartGame()
+    [Header("Main Menu Components")]
+    [SerializeField] private Image mainBackground; // Background image
+    [SerializeField] private Button playButton; // Play button
+    [SerializeField] private Button quitButton; // Quit button
+
+    [Header("Levels Panel Components")]
+    [SerializeField] private GameObject levelsPanel; // Panel for level selection
+    [SerializeField] private Button backButton; // Back button to return to main menu
+
+    [Header("Background Sprites")]
+    [SerializeField] private Sprite mainMenuWithTitle; // Background with title
+    [SerializeField] private Sprite mainMenuClean; // Clean background without title
+
+    #region Unity Methods
+
+    private void Start()
     {
-        // Load the game scene
-        SceneManager.LoadScene(1);
+        // Initialize UI state
+        ReturnToMainMenu();
+
+        // Setup button listeners
+        playButton.onClick.AddListener(ShowLevelsPanel);
+        quitButton.onClick.AddListener(QuitGame);
+        backButton.onClick.AddListener(ReturnToMainMenu);
     }
 
-    public void QuitGame()
+    #endregion
+
+    #region Panel Methods
+
+    private void ShowLevelsPanel()
     {
-        // Quit the application
-        Application.Quit();
-        Debug.Log("Game is quitting...");
+        // Switch to clean background
+        mainBackground.sprite = mainMenuClean;
+
+        // Toggle visibility
+        SetMainMenuElements(false);
+        levelsPanel.SetActive(true);
     }
+
+    public void LoadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    private void ReturnToMainMenu()
+    {
+        // Restore original background
+        mainBackground.sprite = mainMenuWithTitle;
+
+        // Toggle visibility
+        SetMainMenuElements(true);
+        levelsPanel.SetActive(false);
+    }
+
+    #endregion
+
+    #region Main Menu Methods
+
+    private void SetMainMenuElements(bool state)
+    {
+        playButton.gameObject.SetActive(state);
+        quitButton.gameObject.SetActive(state);
+    }
+
+    private void QuitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+
+    #endregion
+
 }
